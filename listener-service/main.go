0eb9fb3148b08ca942253ec624f47099714d03f2
Udit <upandya1297@gmail.com>
2025-01-04 16:40:39 -0500

    rabbitmq.2: rabbitmq connection established

--- listener-service/main.go
+++ listener-service/main.go
@@ -1,11 +1,56 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"time"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
 func main() {
 	// connect to rabbitmq
+	rabbitMQConn, err := connect()
+	if err != nil {
+		log.Println(err)
+		os.Exit(1) // exits program immediately without running `defer` functions
+	}
+	defer rabbitMQConn.Close()
 
 	// listening to messages
 
 	// create consumer
 
 	// watch queue and consume events
+}
+
+func connect() (*amqp.Connection, error) {
+	var counts int64
+	var backOff = 1 * time.Second
+	var connection *amqp.Connection
+
+	// don't continue until rabbitmq is ready
+	for {
+		c, err := amqp.Dial("amqp://guest:guest@localhost")
+		if err != nil {
+			fmt.Println("RabbitMQ not yet ready...")
+			counts++
+		} else {
+			connection = c
+			break
+		}
+
+		if counts > 5 {
+			fmt.Println(err)
+			return nil, err
+		}
+
+		backOff = time.Duration(math.Pow(float64(counts), 2)) * time.Second
+		log.Println("backing off...")
+		time.Sleep(backOff)
+	}
+
+	return connection, nil
 }
